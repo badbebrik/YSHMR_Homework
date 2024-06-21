1524a61988b57b25471425ccf3b4ec85ee1f3f91
Viktoria Serikova <viktoria.serikova2016@yandex.ru>
2024-06-21 08:37:41 +0300

    Initial commit

---
+++ TodoApp/TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Виктория Серикова on 21.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
